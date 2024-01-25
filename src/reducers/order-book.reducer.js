d79ba3c1cb7e106eb56c683e7686d0c6bc1d09df
tnk <rnk@caraminki.com>
2024-01-25 01:02:58 +0100

    WIP: minor bug fix

--- src/reducers/order-book.reducer.js
+++ src/reducers/order-book.reducer.js
@@ -1,21 +1,19 @@
-import { UPDATE_BOOK } from '../constants/constants'
+import { UPDATE_BOOK } from '../constants/constants';
 
-const initial_state = {
-    bids: {}
-    , asks: {}
-    , psnap: {}
-    , mcnt: 0
-}
-function OrderBookReducer(state, action) {
-    if (typeof state === 'undefined') {
-        state = initial_state
-    }
+const initialState = {
+  bids: {},
+  asks: {},
+  psnap: {},
+  mcnt: 0,
+};
 
-    if (action.type === UPDATE_BOOK) {
-        return { ...action.payload }
-    } else {
-        return state
-    }
+function orderBookReducer(state = initialState, action) {
+  switch (action.type) {
+    case UPDATE_BOOK:
+      return { ...action.payload };
+    default:
+      return state;
+  }
 }
 
-export default OrderBookReducer
+export default orderBookReducer;
